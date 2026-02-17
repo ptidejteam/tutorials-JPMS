cd4abaf30453b3bbce98441491db48f83ff0dac2
yann-gael <yann-gael.gueheneuc@concordia.ca>
2026-02-17 08:23:05 -0500

    Minor improvements

--- Project3/src/module-info.java
+++ Project3/src/module-info.java
@@ -1,5 +1,7 @@
 module Module3 {
 	requires Module2;
 	requires org.junit.jupiter.api;
+
+	// Only required to run/pass the test in Maven
 	exports net.ptidej.jpms.module3 to org.junit.platform.commons;
 }
