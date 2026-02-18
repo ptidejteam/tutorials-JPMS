f5c40205c11d1f57262847ab816532f50e4464b5
yann-gael <yann-gael.gueheneuc@concordia.ca>
2026-02-17 22:21:54 -0500

    Brought this branch up-to-date with Ptidej main

---
+++ PADL Creator C++ (ANTLR)/src/test/java/padl/creator/cppfile/antlr/test/TestCreatorCPPFileUsingANTLR.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2001-2014 Yann-Gaël Guéhéneuc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Yann-Gaël Guéhéneuc and others, see in file; API and its implementation
+ ******************************************************************************/
+package padl.creator.cppfile.antlr.test;
+
+import junit.framework.TestSuite;
+import padl.creator.cppfile.antlr.test.all.CpointTest;
+import padl.creator.cppfile.antlr.test.all.Csegment2Test;
+import padl.creator.cppfile.antlr.test.all.InheritanceTest;
+import padl.creator.cppfile.antlr.test.all.WorldTest;
+import padl.kernel.cpp.antlr.ICPPFactoryANTLR;
+import padl.kernel.cpp.antlr.impl.CPPFactoryANTLR;
+
+/**
+ * @author robidose
+ */
+public class TestCreatorCPPFileUsingANTLR extends TestSuite {
+	public static TestSuite suite() {
+		// TODO Remove and move to the tests
+		CppPrimitive
+				.setFactory((ICPPFactoryANTLR) CPPFactoryANTLR.getInstance());
+
+		final TestCreatorCPPFileUsingANTLR suite = new TestCreatorCPPFileUsingANTLR();
+		suite.setName(TestCreatorCPPFileUsingANTLR.class.getName());
+
+		suite.addTestSuite(WorldTest.class);
+		suite.addTestSuite(InheritanceTest.class);
+		suite.addTestSuite(CpointTest.class);
+		suite.addTestSuite(Csegment2Test.class);
+
+		return suite;
+	}
+}
